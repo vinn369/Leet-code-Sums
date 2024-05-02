fd6042af7166fca75340ce0e21ae75fbaca0e85e
vinn369 <153160515+vinn369@users.noreply.github.com>
2024-05-02 12:31:04 +0530

    Create Gas Station.py

---
+++ Gas Station.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        i,j=0,0
+        content_child=0
+        while i<len(g) and j<len(s):
+            if s[j]>=g[i]:
+                content_child+=1
+                i+=1
+            j+=1
+        return content_child
